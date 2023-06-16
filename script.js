56165488bbfd9442202b968acf9a50994c6689d6
Pavlo Schutskiy <99386195+PavloShutz@users.noreply.github.com>
2023-06-16 21:36:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var authForm = document.forms.auth;
+var signup = document.getElementById("signup");
 var tg = window.Telegram.WebApp;
 function sendDataToBot(event) {
     var formData =  new FormData(authForm);
+    let name = formData.get("name");
+    let phone = formData.get("phone");
+    let error = document.getElementById("error");
+    if (name.length < 5)
+    {
+        error.innerHTML = "Name must contain at least 5 characters!"
+        return ;
+    }
+    if (phone.length != 9) {
+        error.innerHTML = "Phone must contain 9 digits!"
+        return ;
+    }
     let data = {
-        name: formData.get("name"),
-        phone: formData.get("phone")
+        name: name,
+        phone: "+380" + phone,
     }
     console.log(data);
     tg.sendData(JSON.stringify(data));
     tg.close();
     event.preventDefault();
 }
-authForm.addEventListener("submit", sendDataToBot);
+signup.addEventListener("click", sendDataToBot);
